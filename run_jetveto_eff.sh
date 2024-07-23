c8d06d7ec5aa9b15a5ca6c9f33d645d40617f251
Shelby Kya Cavanaugh <scavanau@lxplus9103.cern.ch>
2024-07-23 17:56:27 +0200

    added environment

--- run_jetveto_eff.sh
+++ run_jetveto_eff.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+cd CMSSW_14_0_0
+cmsenv
+cd -
 eval$(/afs/cern.ch/user/s/scavanau/vetomaps/jetveto_eff.py)
 printf "Start time: "; /bin/date
 printf "Job is running on node: "; /bin/hostname
